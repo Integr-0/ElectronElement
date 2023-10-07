17df4e2e7da6eb19f9e7cc5e047ee4b99987bd97
luckylukii <lukas.gruenzweil@liwest.at>
2023-10-07 13:25:36 +0200

    Water Shader

--- ElectronElement/Assets/_Project/Scripts/Player/FirstPersonMovement.cs
+++ ElectronElement/Assets/_Project/Scripts/Player/FirstPersonMovement.cs
@@ -5,6 +5,13 @@ public class FirstPersonMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float airSpeed = 8f;
+
+    public float accelerationSpeed = 1f;
+    public float decelerationSpeed = 0.5f;
+
+    [Range(0, 1), Tooltip("(Only for Controller) Will only start moving when the stick is above a certain value from the center")] public float stickDeadzone;
+
     public float gravity = -9.81f;
     public float jump = 1f;
 
@@ -12,8 +19,12 @@ public class FirstPersonMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    Vector3 velocity;
-    bool isGrounded;
+    private Vector3 velocity;
+    private Vector3 movement;
+    private bool isGrounded;
+    private bool isSliding;
+
+    private float accelerationValue = 0;
 
     void Update()
     {
@@ -26,10 +37,19 @@ public class FirstPersonMovement : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        isSliding = Input.GetKey(KeyCode.LeftControl);
 
         Vector3 move = transform.right * x + transform.forward * z;
+        if (move.magnitude > stickDeadzone) movement = move;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (move.magnitude > stickDeadzone) accelerationValue += accelerationSpeed * Time.deltaTime;
+        else if (!isSliding) accelerationValue -= decelerationSpeed * Time.deltaTime;
+
+        accelerationValue = Mathf.Clamp(accelerationValue, 0f, 1f);
+
+        movement *= accelerationValue;
+
+        controller.Move((isGrounded ? speed : airSpeed) * accelerationValue * Time.deltaTime * movement);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
