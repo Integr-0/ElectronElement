c0ca87627deff9b35cec9c641f28306f6fcc8909
luckylukii <lukas.gruenzweil@liwest.at>
2024-01-17 07:45:00 +0100

    Fix visual bug

--- ElectronElement/Assets/_Project/Scripts/GameManager.cs
+++ ElectronElement/Assets/_Project/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -26,15 +27,17 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
             PlayerData data = player.GetComponent<PlayerData>();
             allPlayers.Add(data);
 
-            player.GetComponent<Health>().onDied += async () =>
-            {
-                Popup popup = Instantiate(popupPrefab, popupArea);
-                popup.killedPlayerName = data.Name;
-                popup.killedPlayerImage = data.Image;
-                popup.UpdateUI();
-                await Task.Delay(1000);
-                Destroy(popup);
-            };
+            player.GetComponent<Health>().onDied += () => DisplayPopup(data);
+        }
+
+        async void DisplayPopup(PlayerData data)
+        {
+            Popup popup = Instantiate(popupPrefab, popupArea);
+            popup.killedPlayerName = data.Name;
+            popup.killedPlayerImage = data.Image;
+            popup.UpdateUI();
+            await Task.Delay(1000);
+            Destroy(popup.gameObject);
         }
     }
 
