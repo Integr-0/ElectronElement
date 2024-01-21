abbcf1467fb35980aa35b8a2385afe093da82d4c
luckylukii <lukas.gruenzweil@liwest.at>
2024-01-21 17:25:53 +0100

    bugfixing

--- ElectronElement/Assets/_Project/Scripts/LobbyPreview.cs
+++ ElectronElement/Assets/_Project/Scripts/LobbyPreview.cs
@@ -7,12 +7,18 @@ public class LobbyPreview : MonoBehaviour
     [SerializeField] private TMP_Text mapText;
     [SerializeField] private TMP_Text numPlayersText;
 
+    private string joinCode;
+
     public void Init(string name, string mapName, int numPlayers, int maxPlayers, string joinCode)
     {
         nameText.text = name;
         numPlayersText.text = $"Players: {numPlayers}/{maxPlayers}";
         mapText.text = "Map: " + mapName;
 
+        this.joinCode = joinCode;
+    }
+    public void ShowConfirmationWindow()
+    {
         transform.parent.GetComponent<LobbyPreviewConfirmation>().ShowConfirmWindow(joinCode);
     }
 }
